0325ec5df5d158bb5a4dbc82cc22588786506bfe
Rapax <gasparpelayo44@gmail.com>
2024-07-12 12:21:33 -0300

    ChorizoSeco is born

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,46 @@
+import Head from 'next/head';
+import Image from 'next/image';
 import Link from 'next/link';
 import LaCryptaLogo from '~/components/LaCryptaLogo';
-import Logo from '~/components/Logo';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#d2d2d2] to-[#fefefe]">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight text-black sm:text-[5rem]">
-          <Logo />
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+    <>
+      <Head>
+        <title>Chorizo Seco</title>
+        <meta name="description" content="chorizoseco.com" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="flex min-h-screen flex-col">
+        <div className="background"></div>
+        <main
+          className="b g-gradient-to-b flex min-h-screen flex-col items-center
+      justify-center from-[#d2d2d2] to-[#fefefe]"
+        >
+          <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+            <h1 className="text-5xl font-extrabold tracking-tight text-black sm:text-[5rem]">
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <Image src="/logo.png" alt="Chorizo Seco" width={64} height={64} />
+                <p>Chorizo Seco</p>
+              </div>
+            </h1>
+            <div className="text-lg" style={{ marginTop: '-40px' }}>
+              Tu alias y mail de Bitcoin
+            </div>
+          </div>
+        </main>
+        <footer className="flex h-20 items-center justify-center">
           <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-black hover:bg-white/20"
+            className="flex max-w-xs flex-col gap-4 rounded-xl p-4 text-black"
             href="https://lacrypta.ar"
             target="_blank"
           >
             <h3 className="text-2xl font-bold">
               <LaCryptaLogo />
             </h3>
-            <div className="text-lg">Conocé quiénes están detrás de todo esto.</div>
           </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-10 rounded-xl bg-white/10 p-4 pt-8 text-black hover:bg-white/20"
-            href="https://lawallet.io"
-            target="_blank"
-          >
-            <h3 className="text-3xl font-bold">Desarrolladores</h3>
-            <div className="text-lg">Verificá la documentación, es todo open source.</div>
-          </Link>
-        </div>
+        </footer>
       </div>
-    </main>
+    </>
   );
 }
