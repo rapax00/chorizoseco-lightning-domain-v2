267fdf054eb0a9f6b1f9700acb9d154055bad3e0
Rapax <gasparpelayo44@gmail.com>
2024-07-15 16:19:24 -0300

    Add button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,23 +13,36 @@ export default function Home() {
       </Head>
       <div className="flex min-h-screen flex-col">
         <div className="background"></div>
-        <main
-          className="b g-gradient-to-b flex min-h-screen flex-col items-center
-      justify-center from-[#d2d2d2] to-[#fefefe]"
-        >
-          <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <main className="flex-grow flex justify-center">
+          <div
+            className="container flex flex-col items-center justify-center gap-12 px-4 py-16"
+            style={{ marginTop: '130px' }}
+          >
             <h1 className="text-5xl font-extrabold tracking-tight text-black sm:text-[5rem]">
-              <div style={{ display: 'flex', alignItems: 'center' }}>
-                <Image src="/logo.png" alt="Chorizo Seco" width={64} height={64} />
-                <p>Chorizo Seco</p>
-              </div>
+              <p>Chorizo Seco</p>
             </h1>
             <div className="text-lg" style={{ marginTop: '-40px' }}>
               Tu alias y mail de Bitcoin bien campero.
             </div>
+            <Link
+              className="gap-4 rounded-x1 p-4 text-black"
+              href="https://wallet.chorizoseco.com/signup"
+              target="_blank"
+            >
+              <div
+                style={{ display: 'flex', alignItems: 'center', marginTop: '-40px' }}
+                className="p-4 rounded-xl bg-white/40 hover:bg-black/20"
+              >
+                <Image src="/logo.png" alt="Chorizo Seco" width={32} height={32} />
+                <div className="text-lg">
+                  <b>Quiero un Chorizo Seco</b>
+                </div>
+                <Image src="/logo.png" alt="Chorizo Seco" width={32} height={32} />
+              </div>
+            </Link>
           </div>
         </main>
-        <footer className="flex h-20 items-center justify-center">
+        <footer className="flex h-20 items-center justify-center" style={{ marginBottom: '25px' }}>
           <Link
             className="flex max-w-xs flex-col gap-4 rounded-xl p-4 text-black"
             href="https://lacrypta.ar"
